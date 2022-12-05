598e058848010c8f8d7701499dcf70edc4f70067
Josef Hrebec <josefhrebec02@gmail.com>
2022-12-05 22:07:51 +0100

    Load components through map function

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import React, { useState } from "react";
 import { Card } from "./components/Card";
 
 function App() {
-  return <Card />;
+  const [list, swapList] = useState([
+    { pic: "redux", name: "Redux" },
+    { pic: "openai-icon", name: "OpenAI" },
+    { pic: "astro", name: "Astro" },
+    { pic: "vitejs", name: "Vite" },
+    { pic: "qwik", name: "Qwik" },
+    { pic: "polymer", name: "Polymer" },
+    { pic: "apostrophe", name: "Apostrophe" },
+    { pic: "intercom-icon", name: "Intercom" },
+  ]);
+
+  return (
+    <main className="grid grid-cols-4">
+      {list.map((obj) => {
+        return <Card pic={obj.pic} name={obj.name} key={obj.name} />;
+      })}
+    </main>
+  );
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Icon } from "@iconify/react";
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div className="p-6 rounded-lg bg-slate-300 m-2 w-60 h-60 flex flex-col gap-6 border-solid border-2 border-b-4 border-slate-400">
-      <div className="mx-auto font-semibold text-xl">Polymer</div>
+    <div
+      onClick={props.click}
+      className="p-6 rounded-lg bg-slate-300 m-2 w-60 h-60 flex flex-col gap-6 border-solid border-2 border-b-4 border-slate-400 shadow-md"
+    >
+      <div className="mx-auto font-semibold text-xl">{props.name}</div>
       <div className="p-4 w-40 h-40 bg-white rounded-lg mx-auto flex justify-center items-center border-solid border-2 border-b-4 border-slate-400">
-        <Icon className="mx-auto" icon="logos:polymer" width={100} />
+        <Icon className="mx-auto" icon={`logos:${props.pic}`} width={100} />
       </div>
     </div>
   );
